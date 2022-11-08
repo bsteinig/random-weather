2d770537442473e65b02a53e603c49ea8acb6770
Benjamin Steinig <bsteinig@umich.edu>
2022-11-08 17:17:06 -0500

    Added SWR, started reading about how to use it.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,30 +1,56 @@
-import { Container, Divider, Text, Title } from "@mantine/core";
-
+import {Container, Divider, Text, Title} from '@mantine/core';
+import useSWR from 'swr'
+import { useState, useEffect } from 'react'
 
 export const apiKey = {
-  weatherKey: process.env.NEXT_PUBLIC_WEATHER_API_KEY
-}
+	weatherKey: process.env.NEXT_PUBLIC_WEATHER_API_KEY,
+};
 
 export default function IndexPage() {
-
-  const linkage = ['and', 'but' ]
-
-  // use the api key to fetch data from the weather api
-  const { data, error } = useSWR(
-    `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey.weatherKey}`,
-    fetcher
-  )
-  console.log(data)
+	const fetcher = (...args) => fetch(...args).then(res => res.json())
+	const linkage = ['and', 'but'];
 
 
-  return (
-    <Container fluid sx={(theme) => ({ backgroundColor: theme.colors.blue[4], height: "100vh", padding: '0' })}>
-      <Container fluid sx={(theme) => ({ backgroundColor: theme.colors.cyan[4], height: "50vh" })}>
-      </Container>
-      <Divider mt={0} label={<Title order={3} m={0}>{linkage[0]}</Title>} labelPosition="center" sx={(theme) => ({ position: "absolute", top: "calc(50% - 20.6px / 2)", left: 0, width: "100vw" })} />
-      <Container fluid sx={(theme) => ({ backgroundColor: theme.colors.violet[4], height: "50vh" })}>
+	navigator.geolocation.getCurrentPosition(pos => {
+		console.log("Latitude is :", pos.coords.latitude);
+		console.log("Longitude is :", pos.coords.longitude);
+		let input = {latitude: pos.coords.latitude, longitude: pos.coords.longitude}
+		const { data, error } = useSWR(`${weather_api.base}lat=${input.latitude}&lon=${input.longitude}&units=imperial&appid=${apiKey.weatherKey}`)
+	});
 
-      </Container>
-    </Container>
-  );
+
+	console.log('api-key',apiKey.weatherKey);
+	// use the api key to fetch data from the weather api
+	const {data, error} = useSWR(
+		`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey.weatherKey}`,
+		fetcher,
+	);
+	console.log(data);
+
+	if (error) return <div>failed to load</div>;
+	if (!data) return <div>loading...</div>;
+
+	return (
+		<Container
+			fluid
+			sx={(theme) => ({
+				backgroundColor: theme.colors.blue[4],
+				height: '100vh',
+				padding: '0',
+			})}>
+			<Container
+				fluid
+				sx={(theme) => ({
+					backgroundColor: theme.colors.cyan[1],
+					height: '50vh',
+				})}></Container>
+			
+			<Container
+				fluid
+				sx={(theme) => ({
+					backgroundColor: theme.colors.violet[1],
+					height: '50vh',
+				})}></Container>
+		</Container>
+	);
 }
